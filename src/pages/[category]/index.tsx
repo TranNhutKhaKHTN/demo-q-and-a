2671fddb7d157232257fcc1ccb490d359a290d14
Nhut Kha <trannhutkha123456@gmail.com>
2024-05-14 20:45:16 +0700

    fix buil static category page

--- src/pages/[category]/index.tsx
+++ src/pages/[category]/index.tsx
@@ -13,26 +13,28 @@ const CategoryPage = ({ questions }: any) => {
 export default CategoryPage;
 
 export const getStaticProps = async ({ params }: any) => {
-  // const request = await fetch(`/api/question?category=${params.category}`);
-  // const listQuestion = await request.json();
+  const request = await fetch(
+    `https://demo-q-and-a.vercel.app/api/question?category=${params.category}`
+  );
+  const listQuestion = await request.json();
 
   return {
     props: {
-      // questions: listQuestion,
+      questions: listQuestion,
     },
   };
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  // const request = await fetch("/api/category");
-  // const category = await request.json();
+  const request = await fetch("https://demo-q-and-a.vercel.app/api/category");
+  const category = await request.json();
 
-  // const paths = category.map((item: any) => ({
-  //   params: { category: item.id.toString() },
-  // }));
+  const paths = category.map((item: any) => ({
+    params: { category: item.id.toString() },
+  }));
 
   return {
-    paths: [],
+    paths,
     fallback: false,
   };
 };
